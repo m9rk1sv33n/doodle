c122f4c482dba381324bf07d233f97e9c6cb560f
m9rk1sv33n <funtimes@markisveen.club>
2023-12-21 06:43:15 +0100

    add a mock login screen to toggle users

--- doodle/vite-react-chat-app/src/App.jsx
+++ doodle/vite-react-chat-app/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import ChatApp from "./components/ChatApp";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState("");
+
   return (
     <>
-      <ChatApp />
+      <h1>Chat App for Doodle</h1>
+      {isLoggedIn ? (
+        <>
+          <p className="user"> Logged in as: {user}</p>
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+          <ChatApp />
+        </>
+      ) : (
+        <form className="mock-login-form">
+          <label htmlFor="user">User:</label>
+          <input
+            className="mock-login-form__input"
+            type="text"
+            id="user"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <button
+            className="mock-login-form__button"
+            onClick={() => setIsLoggedIn(true)}
+          >
+            Login
+          </button>
+        </form>
+      )}
     </>
   );
 }
